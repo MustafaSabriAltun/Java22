b816bb7811ca79628061a65549f56b211afd8a09
mustafa altun <altun_ms@hotmail.com>
2022-09-26 15:17:25 +0300

    ilk ödev

---
+++ reCap1/src/reCap1/main.java
@@ -0,0 +1,22 @@
+package reCap1;
+
+public class main {
+
+	public static void main(String[] args) {
+		int sayi1 = 24;
+        int sayi2 = 25;
+        int sayi3 = 2;
+        int enBuyuk = sayi1;
+
+        if(enBuyuk<sayi2){
+            enBuyuk = sayi2;
+        }
+
+        if(enBuyuk<sayi3){
+            enBuyuk = sayi3;
+        }
+
+        System.out.println("En büyük = " + enBuyuk);
+	}
+
+}
